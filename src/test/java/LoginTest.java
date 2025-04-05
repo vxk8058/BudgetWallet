f855450e843ba1135ae8f20e6078da305ba9bea8
zafir99 <148994942+zafir99@users.noreply.github.com>
2025-04-04 21:52:34 -0500

    Add files via upload

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginTest {
+    private DBMgr db = DBMgr.getInstance();
+    private String[] emails = {"johnledford@gmail.com", "whowhatwhere@yahoo.com"};
+    private String[] passwords = {"123456789", "xxyww2"};
+
+    @Test
+    public void TestUC1() {
+        assertEquals(true, db.validUser(emails[0]));
+    }
+
+    @Test
+    public void TestUC2() {
+        assertEquals(false, db.validUser(emails[0], passwords[1]));
+    }
+
+    @Test
+    public void TestUC3() {
+        assertEquals(false, db.validUser(emails[1], passwords[0]));
+    }
+}
